07dd763ba47e27e5c8be441a013932e12dca5637
Egor <egor.kozitski@mail.citytech.cuny.edu>
2015-10-07 12:45:56 -0400

    Card class is implemented and tested

--- src/Card.java
+++ src/Card.java
@@ -9,7 +9,8 @@
  * @author ekozi
  */
 public class Card {
-    public enum Suit{
+   
+   public enum Suit{
       Clubs,Diamonds,Heart,Spades
     };
     private Ranks r;
@@ -40,23 +41,70 @@ public class Card {
    }
   
    public int compareTo(Card c){
-       Suit  suit;
+    int number =0;   
+    //object to be compared
+       Suit  suit2=null;
+       int comparator2 =0;
        switch(c.s){
            case "Clubs":
-                suit = Suit.Clubs ;
+                suit2 = Suit.Clubs ;
+                comparator2=1;
                break;
            case "Diamonds":
-               suit = Suit.Diamonds;
+               suit2 = Suit.Diamonds;
+               comparator2=2;
                break;
            case "Heart":
-               suit = Suit.Heart;
+               suit2 = Suit.Heart;
+                comparator2=3;
                break;
            case "Spades":
-               suit = Suit.Spades;
+               suit2 = Suit.Spades;
+               comparator2=4;
                break;
            default:
                break;
        }
-       if 
+      //super object
+       int comparator1 = 0;
+       Suit suit1 = null;
+      switch(s){
+          case "Clubs":
+                suit1 = Suit.Clubs ;
+                comparator1=1;
+               break;
+           case "Diamonds":
+               suit1 = Suit.Diamonds;
+               comparator1=2;
+               break;
+           case "Heart":
+               suit1 = Suit.Heart;
+                comparator1=3;
+               break;
+           case "Spades":
+               suit1 = Suit.Spades;
+               comparator1=4;
+               break;
+           default:
+               break;
+      }
+     
+        Object o = c;
+        c=(Card)o;
+        System.out.println("The values are "+comparator1+" "+comparator2);
+        if(comparator1==comparator2 && r.getValue()==c.r.getValue())
+            number= 0;
+        //same suit but different number
+        if(comparator1==comparator2 && r.getValue()>c.r.getValue())
+            number= 1;
+         if(comparator1==comparator2 && r.getValue()<c.r.getValue())
+            number =-1;
+          if(comparator1>comparator2)
+            number= 1;
+          if(comparator1<comparator2)
+              return -1;
+              
+           return number;
+     
    }
 }
--- src/TestBinSet.java
+++ src/TestBinSet.java
@@ -13,12 +13,15 @@ public class TestBinSet {
         Ranks r=null;
         Card card = new Card(r.THREE,"Clubs");
         System.out.println(card.toString());
-         Card card1 = new Card(r.THREE,"Clubs");
+         Card card1 = new Card(r.THREE,"Spades");
         System.out.println(card1.toString());
        
-        if(card.equals(card1))
-            System.out.print("They are equals");
-        else
-            System.out.print("They are not equals");
+       if(card.compareTo(card1)==0)
+           System.out.println("They are  equal");
+       else if(card.compareTo(card1)>1)
+           System.out.print("card1 is grater than card2");
+       else{
+                System.out.print("card2 is grater than card1s");
+       } 
     }
 }
